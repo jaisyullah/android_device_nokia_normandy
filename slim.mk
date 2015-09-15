3b6160e5d09c9c937e6c9ba2409197d528f10f76
Ahmed Hady Muhammed <Ahmed-Hady@users.noreply.github.com>
2015-09-15 21:44:39 +0200

    Update slim.mk

--- slim.mk
+++ slim.mk
@@ -1,3 +1,7 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
+
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/slim/config/gsm.mk)
 
